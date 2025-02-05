0aeeb6bffc2dad4de80ffa9d330440802114a619
vignesh r <r.vignesh149@gmail.com>
2025-02-05 22:51:16 +0530

    component update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Mudras from "./mudras/pages/Mudras";
+import NewMudra from "./mudras/pages/NewMudra";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/mudras"  component={Mudras} />
-        <Route path="/mudras/new/"  component={Mudras} />
+        <Route path="/mudras/new/"  component={NewMudra} />
         <Route path="/">
             Hi this is default page
         </Route>
